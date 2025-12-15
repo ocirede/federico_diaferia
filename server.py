bfb9a00eaae88d04a08ac1e3f9367af4b510d0bb
Federico Diaferia <federico.diaferia@gmail.com>
2025-12-15 17:45:26 +0100

    adding linux

--- server.py
+++ server.py
@@ -29,12 +29,12 @@ def portfolio(lang):
     ]
     address = os.getenv("ADDRESS", "")
     address_parts = address.split(".")
-    print(address_parts)
+
     template_vars = {
         "is_mobile": is_mobile,
         "email_parts": [email_user, email_domain_name, email_tld],
         "phone_parts": phone_parts,
-        "address_parts": address_parts
+        "address_parts": address_parts,
     }
 
     if lang == 'de':
